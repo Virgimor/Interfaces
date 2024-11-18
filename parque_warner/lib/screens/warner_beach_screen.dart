ee6e572218372aedca2b807d68c06ddb36eecd0c
Virginia <vigi.mor.98@gmail.com>
2024-11-19 00:42:52 +0100

    Continuacion proyecto warner

--- parque_warner/lib/screens/warner_beach_screen.dart
+++ parque_warner/lib/screens/warner_beach_screen.dart
@@ -10,49 +10,100 @@ class WarnerBeachScreen extends StatefulWidget {
 class _WarnerBeachScreenState extends State<WarnerBeachScreen> {
   @override
   Widget build(BuildContext context) {
+
+    var size = MediaQuery.of(context).size;
+
     return Scaffold(
       appBar: PreferredSize(
         preferredSize: const Size.fromHeight(150),
         child: AppBar(
-          title: Image.asset('assets/images/banner_warner.jpg', height: 1000,),
-          actions:  const [
-            Icon(Icons.add_alert_outlined)
-          ],),
+          flexibleSpace: Container(
+            height: size.height,
+            decoration: const BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage('assets/images/banner_warner.jpg',),
+                fit: BoxFit.fill
+              )
+            ),
+          ),
+          leading: FloatingActionButton(
+            onPressed: () {
+              
+            },
+            backgroundColor: const Color.fromARGB(0, 255, 255, 255),
+            elevation: 0,
+            child: const Icon(Icons.arrow_back_rounded, color: Colors.white,),
+          ),
+          actions:  [
+            FloatingActionButton(
+            onPressed: () {
+              
+            },
+            backgroundColor: const Color.fromARGB(0, 255, 255, 255),
+            elevation: 0,
+            child: const Icon(Icons.add_alert_outlined, color: Colors.white,),
+          ),
+          ],
+        ),
       ),
       body: Column(
         children: [
           Container(
-            decoration: BoxDecoration(
-              border: Border.all(),
-              borderRadius: BorderRadius.circular(20)),
-          ),
-          Container(
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(20)
+            height: size.height * 0.75,
+            decoration: const BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(20),
+                topRight: Radius.circular(20)
+              ),
             ),
-            child: Row(
-              children: [
-                FilledButton(onPressed: () {
-                  
-                }, child: const Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Column(
+                children: [
+                  Row(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisAlignment: MainAxisAlignment.center,
                   children: [
-                    Icon(Icons.search, color: Colors.white,),
-                    Text("Bucar")
+                    FilledButton.icon(onPressed: () {
+                      
+                    },
+                    icon: const Icon(Icons.search, color: Colors.white,),
+                    label: const Text("Bucar"),
+                    style: const ButtonStyle(
+                      backgroundColor: WidgetStatePropertyAll(Color.fromARGB(255, 255, 200, 60))
+                    ),
+                    ),
+                    FilledButton.icon(onPressed: () {
+                      
+                    }, 
+                    icon: const Icon(Icons.confirmation_number_outlined, color: Colors.white,),
+                    label: const Text("Mis entradas"),
+                    style: const ButtonStyle(
+                      backgroundColor: WidgetStatePropertyAll(Color.fromARGB(255, 255, 200, 60))
+                    ),
+                    )
                   ],
-                ) ),
-                FilledButton(onPressed: () {
-                  
-                }, child: const Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Icon(Icons.airplane_ticket),
-                  Text("Mis entradas")
-                  ]
-                ))
-              ],
+                  ),
+                  Column(
+                    children: [
+                      ListView(
+                        children: [
+                          Row(
+
+                          ),
+                          Column(
+
+                          )
+                        ],
+                      )
+                    ]
+                  )
+                ],
+              ),
             ),
-          )
+            
+          ),
         ],
       ),
     );
